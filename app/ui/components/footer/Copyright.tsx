570474caa9de5b7f49fcf7b81b03e86334953f4a
Etan Joseph Heyman <etan@heyman.net>
2024-03-03 10:21:39 -0600

    Fix footer

--- app/ui/components/footer/Copyright.tsx
+++ app/ui/components/footer/Copyright.tsx
@@ -1,10 +1,37 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Copyright = () => {
   return (
     <h1 className='flex-1 text-3xl font-normal text-[#680995] lg:text-xl'>
-      © 2024 Weby. Developed by Etan Heyman, Itsik Bamani, and Natalie Gariba.
-      All rights reserved.
+      © 2024 Weby. Designed by{' '}
+      <Link
+        className='cursor-pointer hover:underline'
+        target='_blank'
+        aria-description='UX/UI Designer Julia Voronina`s linkedin profile'
+        href='https://www.linkedin.com/in/julia-voronina-660a6a28b/'
+      >
+        Julia Voronina
+      </Link>
+      . Developed by{' '}
+      <Link
+        className='cursor-pointer hover:underline'
+        target='_blank'
+        aria-description='Full stack developer Etan Heyman`s linkedin profile'
+        href='https://www.linkedin.com/in/etanjheyman/'
+      >
+        Etan Heyman
+      </Link>
+      ,{' '}
+      <Link
+        className='cursor-pointer hover:underline'
+        target='_blank'
+        aria-description='Full stack developer Itsik Bamani`s linkedin profile'
+        href='https://www.linkedin.com/in/itsik-bamani-9b5a8b1b6/'
+      >
+        Itsik Bamani
+      </Link>{' '}
+      and Natalie Gariba. All rights reserved.
     </h1>
   );
 };
--- app/ui/components/footer/index.tsx
+++ app/ui/components/footer/index.tsx
@@ -5,7 +5,7 @@ import Copyright from './Copyright';
 
 const Footer = () => {
   return (
-    <footer className='z-[90] flex w-full flex-shrink-0 flex-col gap-y-7 bg-white/80 px-8 pb-12 pt-4 md:px-32 md:py-8 lg:flex-row lg:gap-x-11 lg:py-9'>
+    <footer className='z-[90] flex w-full flex-shrink-0 flex-col items-center gap-y-7 bg-white/80 px-8 pb-12 pt-4 md:px-32 md:py-8 lg:flex-row lg:gap-x-11 lg:py-9'>
       <div className='flex w-full items-center justify-between gap-16 md:w-fit'>
         <WebysLogo />
         <SocialMediaIcons />
