917d0db220fcff1be05d719c1bda1b6f93e98462
Etan Joseph Heyman <etan@heyman.net>
2024-03-12 12:14:08 -0500

    Layout shift in hero section

--- app/ui/components/sections/hero-section/index.tsx
+++ app/ui/components/sections/hero-section/index.tsx
@@ -10,7 +10,7 @@ const HeroSection = ({
 }) => {
   return (
     <div
-      className={`relative  flex h-fit flex-col items-center justify-center gap-10  overflow-x-clip max-lg:w-full md:px-6 md:py-20 lg:items-start lg:px-24 xl:px-48 xl:pr-56 2xl:max-w-screen-desktop 2xl:items-center 2xl:place-self-center`}
+      className={`relative  flex h-fit flex-col items-center justify-center gap-10  overflow-x-clip max-lg:w-full sm:ml-10  md:px-6 md:py-20 lg:items-start lg:px-24 xl:px-48 xl:pr-56 2xl:max-w-screen-desktop 2xl:items-center 2xl:place-self-center`}
     >
       <div className='absolute left-8 top-[52px] md:hidden'>
         <Logo />
